a0a4f8c797de3ffa3fb6e4ad05c75dcb94043e50
Alex <93647915+nachprod2016@users.noreply.github.com>
2023-04-06 21:09:03 +0300

    Add files via upload

---
+++ src_server/Server.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <map>
+#include <iostream>
+#include <unistd.h>
+#include <iterator>
+#include <list>
+#include "DataBase.h"
+#include "../src_shared/Socket.h"
+
+#define PORT 7777
+
+class Server
+{
+private:
+    int fdServerSocket, fdClientSocket;
+    struct sockaddr_in  serverAddress, clientAddress;
+    socklen_t length;
+    bool flag;
+    std::string activeUser;
+    Socket* sckt;
+    DataBase db;
+public:
+    Server();
+    ~Server() = default;
+    void listenPort();
+    void runInstruction(int instruction);
+    void registration();
+    void authorization();
+    void sendUsersList();
+    void sendChatsList();
+    void addNewChat();
+    void addNewMessage();
+    void sendMessagesList();
+};
---
+++ src_server/main_s.cpp
@@ -0,0 +1,8 @@
+#include "Server.h"
+
+int main()
+{
+    Server sr;
+    sr.listenPort();
+    return 0;
+}
